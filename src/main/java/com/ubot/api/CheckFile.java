f40774f4155c7036c27659394bacf2549ebc8a05
Taehyaung <10846011@ntub.edu.tw>
2021-08-18 15:21:25 +0800

    檢查檔案是否可進行上傳，socket client端傳送檔案

--- src/main/java/com/ubot/api/CheckFile.java
+++ src/main/java/com/ubot/api/CheckFile.java
@@ -1,7 +1,29 @@
 package com.ubot.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 
 public class CheckFile extends Thread {
 	public CheckFile() {
@@ -10,12 +32,103 @@ public class CheckFile extends Thread {
 	@Override
 	public void run() {
 		try {
-			ServerSocket ss = new ServerSocket(8000);
 			while (true) {
-				System.out.println("Socket open");
-				Socket s = ss.accept();
-				Thread t = new Thread(new SocketServer(s));
-				t.start();
+				String dirName = "/Users/yangzhelun/Desktop/uploadFile";
+				String zipName = "/Users/yangzhelun/Desktop/zipFile/";
+				List filePaths = new ArrayList();
+				Files.list(new File(dirName).toPath()).forEach(path -> {
+					filePaths.add(path);
+				});
+				if (filePaths.size() == 1) {
+					System.out.println("目前資料夾為空的，不能檢查");
+				}
+				for (int i1 = 1; i1 < filePaths.size(); i1++) {
+					String name = filePaths.get(i1).toString();
+					List fileCount = new ArrayList();
+					Files.list(new File(name).toPath()).forEach(path -> {
+						System.out.println(name);
+						fileCount.add(path);
+					});
+					if (fileCount.size() >= 8) {
+						String newFileName = name.split("uploadFile/")[1];
+						Path sourceFolderPath = Paths.get(name);
+						Path zipPath = Paths.get(zipName + newFileName + ".zip");
+						System.out.println(name + "可以上傳摟");
+						ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath.toFile()));
+						Files.walkFileTree(sourceFolderPath, new SimpleFileVisitor<Path>() {
+							public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+								zos.putNextEntry(new ZipEntry(sourceFolderPath.relativize(file).toString()));
+								Files.copy(file, zos);
+								zos.closeEntry();
+								return FileVisitResult.CONTINUE;
+							}
+						});
+						zos.close();
+						File uploadFile = new File(zipName + newFileName + ".zip");
+						FileInputStream input = new FileInputStream(uploadFile);
+						byte[] content = null;
+						try {
+							content = Files.readAllBytes(zipPath);
+						} catch (final IOException e) {
+						}
+						ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+						Future future = executor.submit(new  ClientThreadCode(uploadFile));
+						executor.schedule(new Runnable(){
+						    public void run(){
+						        future.cancel(true);
+						        System.out.println("上傳終止");
+						    }
+						}, 30000, TimeUnit.MILLISECONDS); //設置上傳時數限制，若超出則強致中斷
+						executor.shutdown();
+						new CheckFile().join();
+						File f = new File(name); // file to be delete
+						String[] entries = f.list();
+						for (String s : entries) {
+							File currentFile = new File(f.getPath(), s);
+							currentFile.delete(); // 開始刪除檔案
+						}
+						try {
+							if (f.delete()) // returns Boolean value
+							{
+								System.out.println(f.getName() + " deleted"); // getting and printing the file name
+							} else {
+								System.out.println("failed");
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+					} else {
+						File myfile = new File(name);
+						Path path = myfile.toPath();
+						BasicFileAttributes fatr = Files.readAttributes(path, BasicFileAttributes.class);
+						Clock clock = Clock.systemUTC();
+						Instant instant = clock.instant();
+						long duration = ChronoUnit.MINUTES.between(fatr.lastModifiedTime().toInstant(), instant);
+						if (duration > 300) {
+							String[] entries = myfile.list();
+							for (String s : entries) {
+								File currentFile = new File(myfile.getPath(), s);
+								currentFile.delete();
+							}
+							try {
+								if (myfile.delete()) // returns Boolean value
+								{
+									System.out.println(myfile.getName() + " deleted"); // getting and printing the file
+																						// name
+								} else {
+									System.out.println("failed");
+								}
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+						System.out.println(name + "還不能上傳");
+					}
+					fileCount.clear();
+				}
+				filePaths.clear();
+				Thread.sleep(3000);
 			}
 		} catch (Exception e) {
 		}
---
+++ src/main/java/com/ubot/api/ClientThreadCode.java
@@ -0,0 +1,52 @@
+package com.ubot.api;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.Socket;
+
+public class ClientThreadCode extends Thread {
+	File clientFile;
+	private FileInputStream fis;
+	private DataOutputStream dos;
+
+	public ClientThreadCode(File file) {
+		clientFile = file;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println("upload started");
+			Socket s = new Socket("172.20.10.10", 8000);
+			if (clientFile.exists()) {
+				fis = new FileInputStream(clientFile);
+				dos = new DataOutputStream(s.getOutputStream());
+				dos.writeUTF(clientFile.getName());
+				dos.flush();
+				dos.writeLong(clientFile.length());
+				dos.flush();
+				System.out.println("======== 開始傳輸檔案 ========");
+				byte[] bytes = new byte[(int) clientFile.length()];
+				int length = 0;
+				long progress = 0;
+				while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
+					dos.write(bytes, 0, length);
+					dos.flush();
+					progress = length;
+					System.out.print("| " + (100 * progress / clientFile.length()) + "% |");
+				}
+				System.out.println();
+				System.out.println("======== 檔案傳輸成功 ========");
+				fis.close();
+				dos.close();
+				s.close();
+			} else {
+				System.out.println("file not exists");
+			}
+		} catch (Exception e) {
+			System.out.println("Error occur :" + e);
+		}
+	}
+
+}
