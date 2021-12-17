ed6442f9d1c63303b67532b8300660306e889382
azhe922 <zsda5858sda@gmail.com>
2021-12-17 17:12:53 +0800

    新增cors相關設定

--- src/main/java/com/ubot/MainApp.java
+++ src/main/java/com/ubot/MainApp.java
@@ -2,6 +2,7 @@ package com.ubot;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.ubot.config.Config;
 import com.ubot.config.XSSFilter;
 
 import jakarta.ws.rs.ApplicationPath;
@@ -13,6 +14,7 @@ public class MainApp extends ResourceConfig {
 	public MainApp() {
 		// 用來掃描該package裡所有@Path並註冊
 		packages("com.ubot.api");
+		register(Config.class);
 		register(XSSFilter.class);
 	}
 
---
+++ src/main/java/com/ubot/config/Config.java
@@ -0,0 +1,24 @@
+package com.ubot.config;
+
+import java.io.IOException;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.container.PreMatching;
+import jakarta.ws.rs.ext.Provider;
+
+// 在http request進來後第一個進來的class，之後才會pass到service
+@Provider
+@PreMatching
+public class Config implements ContainerResponseFilter {
+
+	public void filter(ContainerRequestContext req, ContainerResponseContext res) throws IOException {
+		// 設定可使用來源
+		res.getHeaders().add("Access-Control-Allow-Origin", "*");
+		res.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		res.getHeaders().add("Access-Control-Allow-Headers", "*");
+		res.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
+	}
+
+}
